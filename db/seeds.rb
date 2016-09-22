76caaa1d0184c4f9ef750d5e835e1c1e2240e181
hathbanger <hathbanger.ah@gmail.com>
2016-09-22 12:05:35 -0600

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 f = Fish.new(name: 'nemo')
+f2 = Fish.new(name: 'dory')
+f3 = Fish.new(name: 'jaws')
+f4 = Fish.new(name: 'flipper')
+
 f.save
+f2.save
+f3.save
+f4.save
 
 u1 = User.create!(email: "hathbanger.ah@gmail.com", password: "amh05055", password_confirmation: "amh05055")
 u2 = User.create!(email: "matthewberryhill@gmail.com", password: "rpm80203", password_confirmation: "rpm80203")
 
 u1.fish << f
+u1.fish << f2
+u2.fish << f3
+u2.fish << f4
